a5e8dae4f5c25fb19fc59091a86def7e7d33bcc1
icnsat <144019284+icnsat@users.noreply.github.com>
2024-04-04 23:00:57 +0300

    Delete pract_7 directory

--- pract_7/Decorator/CafeClient.java
+++
@@ -1,7 +0,0 @@
-package sem_4.pract_7.Decorator;
-
-public class CafeClient implements Client{
-    public String eat(){
-        return "Eating food... Food is eaten.";
-    }
-}
--- pract_7/Decorator/Client.java
+++
@@ -1,7 +0,0 @@
-package sem_4.pract_7.Decorator;
-
-public interface Client {
-    String eat();
-}
-
-
